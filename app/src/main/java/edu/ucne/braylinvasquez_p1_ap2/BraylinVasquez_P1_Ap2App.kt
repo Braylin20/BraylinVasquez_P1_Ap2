054f9d1fb4a601f1ab9bce4a7d64000766b731f9
Braylin <braylinvasquez20@gmail.com>
2024-09-18 19:58:36 -0400

    Implementacion de Hilt

---
+++ app/src/main/java/edu/ucne/braylinvasquez_p1_ap2/BraylinVasquez_P1_Ap2App.kt
@@ -0,0 +1,8 @@
+package edu.ucne.braylinvasquez_p1_ap2
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class BraylinVasquez_P1_Ap2App:Application() {
+}

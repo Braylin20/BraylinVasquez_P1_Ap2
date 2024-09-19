963e3219f46480f9aad91dac4c17194ddbe8af72
Braylin <braylinvasquez20@gmail.com>
2024-09-18 21:06:25 -0400

    Agrega Base de datos, entity y dao

---
+++ app/src/main/java/edu/ucne/braylinvasquez_p1_ap2/data/local/dao/AlgoDao.kt
@@ -0,0 +1,23 @@
+package edu.ucne.braylinvasquez_p1_ap2.data.local.dao
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Query
+import androidx.room.Upsert
+import edu.ucne.braylinvasquez_p1_ap2.data.local.entities.AlgoEntity
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface AlgoDao {
+    @Upsert
+    suspend fun save(algo: AlgoEntity)
+
+    @Query("Select * from Algos where algoId = :id")
+    suspend fun find(id: Int): AlgoEntity
+
+    @Delete
+    suspend fun delete(algo: AlgoEntity)
+
+    @Query("Select * from Algos")
+    suspend fun getAll(): Flow<List<AlgoEntity>>
+}
---
+++ app/src/main/java/edu/ucne/braylinvasquez_p1_ap2/data/local/database/Parcial1DataBase.kt
@@ -0,0 +1,19 @@
+package edu.ucne.braylinvasquez_p1_ap2.data.local.database
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import edu.ucne.braylinvasquez_p1_ap2.data.local.dao.AlgoDao
+import edu.ucne.braylinvasquez_p1_ap2.data.local.entities.AlgoEntity
+
+
+@Database(
+    entities = [
+        AlgoEntity::class
+    ],
+    version = 1,
+    exportSchema = false,
+
+)
+abstract class Parcial1DataBase:RoomDatabase() {
+    abstract fun algoDao(): AlgoDao
+}
